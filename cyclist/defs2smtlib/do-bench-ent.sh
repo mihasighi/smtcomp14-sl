592216eafb665859d5e1e4e09b206e789e923844
Nikos <nikos.gorogiannis@gmail.com>
2014-05-31 21:54:10 +0100

    Forgot to add changed files to the index.

---
+++ cyclist/defs2smtlib/do-bench-ent.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Execute
+#  do-bench.sh <srcdir> <prefix-file> <root> <dst-dir>
+#
+
+CYCTOOL=slsat_check.native
+
+SRCDIR=$1
+BENCH=$2
+ROOT=$3
+DSTDIR=$4
+
+for i in `ls ${SRCDIR}/${BENCH}*.defs`
+do
+	echo "---- Translate file \"${i}\" root ${ROOT}"
+	cp ${i} tmp.defs
+        sed -i 's/and/andg/g' tmp.defs
+        sed -i 's/xor/xorg/g' tmp.defs
+        sed -i 's/not/notg/g' tmp.defs
+	${CYCTOOL} -r ${ROOT} -D tmp.defs
+        FILE=`basename ${i}`
+	FILE=${DSTDIR}/${FILE}.smt2
+	cat set-info.ent.smt2 assert.smt2 info.smt2 &> ${FILE} 
+	echo "   into ${FILE}"
+done
+
+
---
+++ cyclist/defs2smtlib/set-info.sat.smt2
@@ -0,0 +1,13 @@
+(set-logic QF_S)
+(set-info :source |
+  James Brotherston, Carsten Fuhs, Nikos Gorogiannis, and Juan Navarro Pérez. 
+  A decision procedure for satisfiability in separation logic with inductive 
+  predicates. To appear at CSL-LICS, 2014. 
+  https://github.com/ngorogiannis/cyclist/releases/tag/CSL-LICS14
+|)
+(set-info :smt-lib-version 2.0)
+(set-info :category "crafted")
+(set-info :status unknown)
+(set-info :version 2014-05-31)
+
+
